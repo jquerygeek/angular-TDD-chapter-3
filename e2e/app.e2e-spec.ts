7a8b8d160f0ae81a37472caec600ed852dbf0a2c
Md.Ziaul Haq <jquerygeek@cloud.upwork.com>
2017-02-09 10:16:11 +0600

    Update protractor and test case

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,15 +1,13 @@
 import { browser, element, by } from 'protractor';
 
-describe('QuickStart E2E Tests', function () {
+describe('AppComponent Tests', () => {
 
-  let expectedMsg = 'Hello Angular';
+    beforeEach(() => {
+        browser.get('/');
+    });
 
-  beforeEach(function () {
-    browser.get('');
-  });
-
-  it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
-  });
+    it('Browser should have a defined title', () => {
+        expect(browser.getTitle()).toEqual('Angular Karma');
+    });
 
 });
